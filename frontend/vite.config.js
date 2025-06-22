7f401ae31513573d08327aa19ba82f57f0cd5293
Voonaa <agusss1618@gmail.com>
2025-06-23 03:36:01 +0700

    vercel hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
   },
   build: {
     outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser'
   },
 }); 
